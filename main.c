4a6b86523eba4ca2f1de0c36ea4d4082bab46d2c
Philippe <nphilippe40@gmail.com>
2023-01-04 15:09:50 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+void AfficheHexa(char* msg, int tailleTab, char* msgAEnvoye);
 
 int main()
 {
-    printf("Hello World!\n");
-    return 0;
+  unsigned char message[]={0x0F, 0x44, 0x0B, 0x3D}; // initialisation
+  AfficheHexa(message, 4, "Message a envoyer"); // on passe une
+   // chaine de caractères correspondant au nom du tableau
+   return 0 ;
+}
+
+void AfficheHexa(char* msg, int tailleTab, char* msgAEnvoye){
+
+  printf("%s [%d] : ", msgAEnvoye, tailleTab);
+
+  for(int i=0;i<tailleTab;i++){
+
+    printf("%x ",msg[i]);
+
+  }
+
 }
